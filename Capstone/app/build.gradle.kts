7e8076ae38395000de963bc3c738ad4ddc24c3ba
myatminhtoo <myatminhtoo1@gmail.com>
2025-08-02 00:49:08 -0500

    Added api id to variable, implement url to connect with api website

--- Capstone/app/build.gradle.kts
+++ Capstone/app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
@@ -15,6 +17,9 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        val secretProps = rootProject.extra["secretProperties"] as Properties
+        buildConfigField("String", "API_KEY", "\"${secretProps["API_KEY"]}\"")
     }
 
     buildTypes {
@@ -26,6 +31,11 @@ android {
             )
         }
     }
+
+    buildFeatures {
+        buildConfig = true  // Enable BuildConfig generation
+    }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
@@ -36,12 +46,13 @@ android {
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
     implementation(libs.androidx.constraintlayout)
+    implementation("com.google.android.gms:play-services-maps:19.2.0")
+    implementation("com.google.android.libraries.places:places:4.4.1")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- Capstone/app/src/main/java/com/codepath/capstone/MainActivity.kt
+++ Capstone/app/src/main/java/com/codepath/capstone/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.codepath.capstone
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +10,20 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val apiKey = "13a395d09e47268c9895eb8684cafac24b4e9dc9749058c9884a9f4b4e416b12"
+        val query = "restaurants"
+        val location = "New York, NY"
+
+        val url = "https://serpapi.com/search.json?engine=google_local&query=$query&location=$location&api_key=$apiKey"
+
+        Log.d("API_KEY", "API Key: $apiKey")
+        }
     }
-}
