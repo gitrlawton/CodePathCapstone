7dc9b80d8167dc0a2a81031f9216c93203aefad6
myatminhtoo <myatminhtoo1@gmail.com>
2025-08-03 17:54:01 -0500

    Fixed navigation bar icons and texts misalignment Used custom colors in place of material icons and texts (selected & not-selected) Fixed the gap between nav bar and android ui gesture bar in MainActivity.kt.

--- Capstone/app/src/main/java/com/codepath/capstone/MainActivity.kt
+++ Capstone/app/src/main/java/com/codepath/capstone/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.codepath.capstone
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        val mainLayout = findViewById<View>(R.id.main)
+        val bottomNavBar = findViewById<View>(R.id.bottomNavBar)
+
+        ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, 0)
+            insets
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(bottomNavBar) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updatePadding(bottom = systemBars.bottom)
             insets
         }
     }
