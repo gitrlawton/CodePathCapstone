738c8d8d83bf432a3ba5bdacf42563bc85efdb23
Alex Surprenant <102190687+Soup098@users.noreply.github.com>
2025-08-03 21:46:47 -0600

    update

--- Capstone/app/src/main/java/com/codepath/capstone/MainActivity.kt
+++ Capstone/app/src/main/java/com/codepath/capstone/MainActivity.kt
@@ -2,20 +2,26 @@ package com.codepath.capstone
 
 import android.os.Bundle
 import android.view.View
+import android.view.LayoutInflater
+import android.widget.FrameLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.updatePadding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var container: FrameLayout
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
         val mainLayout = findViewById<View>(R.id.main)
-        val bottomNavBar = findViewById<View>(R.id.bottomNavBar)
+        val bottomNavBar = findViewById<BottomNavigationView>(R.id.bottomNavBar) //changed this line to use BottomNavigationView. gives setOnItemSelectedListener method to bottomNavBar -Alex
+        container = findViewById(R.id.fragment_container)
 
         ViewCompat.setOnApplyWindowInsetsListener(mainLayout) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -28,5 +34,29 @@ class MainActivity : AppCompatActivity() {
             v.updatePadding(bottom = systemBars.bottom)
             insets
         }
+    //*** code for giving functionality to the nav bar WORK IN PROGRESS -Alex
+        loadLayout(R.layout.activity_main)
+
+        bottomNavBar.setOnItemSelectedListener { item ->
+            when (item.itemId){
+                R.id.nav_home -> {
+                    loadLayout(R.layout.activity_main)
+                    true
+                }
+                R.id.nav_search -> {
+                    loadLayout(R.layout.search_component)
+                    true
+                }
+                else -> false
+            }
+        }
     }
+
+    private fun loadLayout(layoutResId: Int){
+        container.removeAllViews()
+        val view = LayoutInflater.from(this).inflate(layoutResId, container, false)
+        container.addView(view)
+    }
+
+    //*** End of nav bar functionality code block ******
 }
